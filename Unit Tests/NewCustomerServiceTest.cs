807d111a4ca14a81cb5f18929d99021ee71d0269
Myroslava Movchan <myroslava.mov@gmail.com>
2024-12-12 01:31:42 +0200

    new customer service unit test

---
+++ Unit Tests/NewCustomerServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Online_Store_Management.Services;
+
+namespace Unit_Tests
+{
+    [TestClass]
+    public sealed class NewCustomerServiceTest
+    {
+        private NewCustomerService service;
+
+        [TestMethod]
+        public async Task GetNewCustomerAsyncTest_ShouldReturnNewCustomer()
+        {
+            //Arrange
+            service = new NewCustomerService();
+            var cancellationToken = CancellationToken.None;
+
+            //Act
+            var result = service.GetNewCustomerAsync(cancellationToken);
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
