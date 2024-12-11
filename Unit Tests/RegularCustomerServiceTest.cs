c25f895390af34288c1f6a94dd1c2e7299dd99ad
Myroslava Movchan <myroslava.mov@gmail.com>
2024-12-12 01:14:36 +0200

    regular customer service unit test

---
+++ Unit Tests/RegularCustomerServiceTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Online_Store_Management.Services;
+
+namespace Unit_Tests
+{
+    [TestClass]
+    public sealed class RegularCustomerServiceTest
+    {
+        private RegularCustomerService service;
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            service = new RegularCustomerService();
+        }
+
+        [TestMethod]
+        public async Task GetRegularCustomerAsyncTest_ShouldReturnRegularCustomer()
+        {
+            //Arrange
+            var cancellationToken = CancellationToken.None;
+
+            //Act
+            var result = service.GetRegularCustomerAsync(cancellationToken);
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
